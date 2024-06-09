da311ad7e10a68d81ac859c035eb72ad01b702bf
Huijong JEONG <huijong.jeong@squeezebits.com>
2024-06-09 11:35:00 +0900

    let's get rusty

---
+++ ch1/hello-world/main.rs
@@ -0,0 +1,6 @@
+fn main() { // main function 
+    // function body, indentation done with 4 spaces
+    println!("Hello, world!"); // ! at the end of the function name indicates that it is a macro, not a normal function
+
+}
+
